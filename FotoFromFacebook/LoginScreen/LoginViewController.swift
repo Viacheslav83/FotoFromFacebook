d6f9af105a72208a995d571fb6fad4075b3eb68e
Viacheslav <markvyach83@gmail.com>
2021-03-24 15:20:12 +0200

    created Login and Present Screens

--- FotoFromFacebook/LoginScreen/LoginViewController.swift
+++ FotoFromFacebook/LoginScreen/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  FotoFromFacebook
 //
 //  Created by Viacheslav Markov on 24.03.2021.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
-
